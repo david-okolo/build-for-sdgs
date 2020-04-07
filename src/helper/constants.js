f0655eaf9a7ffea91ff959a6af80d4cb3cd92844
David Okolo <david.okolo@icloud.com>
2020-04-07 19:58:51 +0100

    add implementation for impact estimator

---
+++ src/helper/constants.js
@@ -0,0 +1,14 @@
+/**
+ * Keep generic string or number constants in a file and export.
+ * Solves any string based inconsistencies in application code or tests.
+ */
+
+const days = 'days';
+const weeks = 'weeks';
+const months = 'months';
+
+export default {
+  days,
+  weeks,
+  months
+};
