8f9bb53dd11542ea165629e776bc9ac776ce76b0
David Okolo <david.okolo@icloud.com>
2020-04-14 21:03:56 +0100

    add pm2 ecosystem file and add ssl certificate

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 /**
  * Server file
  */
-
+import fs from 'fs';
+import https from 'https';
 import app from './app';
 
 const port = process.env.PORT || 3000;
 
-// starts the server
-app.listen(port);
+if (process.env.NODE_ENV === 'production') {
+  // starts the server
+  https.createServer({
+    key: fs.readFileSync('../cert/server-key.pem'),
+    cert: fs.readFileSync('../cert/server-cert.pem')
+  }, app).listen(port);
+} else {
+  // starts the server
+  app.listen(port);
+}
