ecf2151dff279977853ba838b259c9df34472bf5
David Okolo <david.okolo@icloud.com>
2020-04-14 21:11:09 +0100

    fix certificate files location

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 3000;
 if (process.env.NODE_ENV === 'production') {
   // starts the server
   https.createServer({
-    key: fs.readFileSync('../cert/server-key.pem'),
-    cert: fs.readFileSync('../cert/server-cert.pem')
+    key: fs.readFileSync('./cert/server-key.pem'),
+    cert: fs.readFileSync('./cert/server-cert.pem')
   }, app).listen(port);
 } else {
   // starts the server
