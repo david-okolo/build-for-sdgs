7c9c7e8216a312d864a1f9fcb89dcad255c36c98
David Okolo <david.okolo@icloud.com>
2020-04-26 16:50:32 +0100

    add toggle ability

--- public/app.js
+++ public/app.js
@@ -1,10 +1,29 @@
 window.addEventListener('load', () => {
+  let buttonState = false;
   const toggleCircle = document.querySelector('.circle');
   const toggleRounded = document.querySelector('.rounded-rectangle');
-  toggleCircle.addEventListener('click', () => {
+
+  const turnOn = () => {
     toggleCircle.style.transition = 'all 0.5s';
     toggleCircle.style.transform = 'translateX(60px)';
     toggleRounded.style.transition = 'all 0.2 ease 0.4';
     toggleRounded.style.backgroundColor = '#2AC999';
+  };
+
+  const turnOff = () => {
+    toggleCircle.style.transition = 'all 0.5s';
+    toggleCircle.style.transform = 'translateX(0px)';
+    toggleRounded.style.transition = 'all 0.2 ease 0.4';
+    toggleRounded.style.backgroundColor = 'grey';
+  };
+
+  toggleCircle.addEventListener('click', () => {
+    if(buttonState)
+    {
+      turnOff();
+    } else {
+      turnOn();
+    }
+    buttonState = !buttonState;
   });
 });
