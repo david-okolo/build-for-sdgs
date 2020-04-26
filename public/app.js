ba8e6223c4393ce189d15c0d7d6622aad44119ca
David Okolo <david.okolo@icloud.com>
2020-04-26 17:54:53 +0100

    add ranges

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
 
   const turnOn = () => {
     toggleCircle.style.transition = 'all 0.5s';
-    toggleCircle.style.transform = 'translateX(60px)';
+    toggleCircle.style.transform = 'translateX(30px)';
     toggleRounded.style.transition = 'all 0.2 ease 0.4';
     toggleRounded.style.backgroundColor = '#2AC999';
   };
